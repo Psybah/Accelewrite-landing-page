a9502bb30cf421554e5a4d86f2c6381afed2dd8e
Abiodun Oluwamurewa <abiodunoluwamurewa@gmail.com>
2024-12-19 12:16:20 +0100

    Create testimonialData.ts

---
+++ src/components/testimonials/testimonialData.ts
@@ -0,0 +1,42 @@
+export const testimonials = [
+  {
+    name: "Awe Sanmi",
+    quote: "Accelewrite has given me more education, motivation, community, and confidence about career opportunities than years of anything else."
+  },
+  {
+    name: "Faniyi Damilare",
+    quote: "Starting to write online was the single best professional decision I've ever made, by an enormous margin."
+  },
+  {
+    name: "Ronke Salami",
+    quote: "Many writing programs promise to make you a better writer. Accelewrite goes beyond to make you a better human. It didn't just unlock my pen, it changed my heart."
+  },
+  {
+    name: "Abasi Idara Etim",
+    quote: "The genuine enthusiasm and support of Accelewrite has been a gift to my writing. I didn't think it possible to write what I did, but there it was on the page."
+  },
+  {
+    name: "Habeebah Musbau",
+    quote: "Accelewrite has taught me more useful skills than I could have ever dreamt! The camaraderie, the small groups, the feedback are all outstanding."
+  },
+  {
+    name: "Ajiboye Charity",
+    quote: "Accelewrite shows you how to be prolific in writing, thoughts, and publishing. It's changed my life."
+  },
+  {
+    name: "Karim Justin",
+    quote: "Accelewrite gave me a structure and a community to support my creative process. And because of the friends I've made, I feel more intellectually and creatively stimulated than I have since university."
+  },
+  {
+    name: "Karen Akinfolami",
+    quote: "It got me over my biggest hurdle: fear of publishing. In week one, I was scared to hit publish. By week five, it felt natural. I would not have gotten there without this program."
+  },
+  {
+    name: "Kojo Onaolapo",
+    quote: "I can't believe an online course that actually under promises and over delivers. I literally 'found my people.'"
+  },
+  {
+    name: "Kelvin Ogunniyi",
+    quote: "Accelewrite has opened doors for me that otherwise wouldn't be possible to even knock on. It will forever be my favorite place on the Internet."
+  }
+];
