6d994987b6efa42d692cd2f78eaa3a0ee9b874b0
Abiodun Oluwamurewa <abiodunoluwamurewa@gmail.com>
2024-12-19 12:54:48 +0100

    Update Stats.tsx

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -24,7 +24,7 @@ const stats = [
 
 export default function Stats() {
   return (
-    <div className="bg-gradient-to-b from-[#121b54] to-white py-20">
+   <div className="bg-gradient-to-b from-[#ffffff] via-[#d9e1f2] to-white py-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">
